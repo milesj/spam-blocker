8de839a6790dc4c455c70456343dc72d103e0690
Miles Johnson <mileswjohnson@gmail.com>
2010-08-15 23:02:01 -0700

    Renamed from Commentia to Spam Blocker. Added support for slugged article URLs. Removed the EmailComponent and used mail(). Renamed a few variables and properties.

--- models/behaviors/spam_blocker.php
+++ models/behaviors/spam_blocker.php
@@ -1,27 +1,43 @@
 <?php
 /** 
- * Commentia Behavior - Comment Spam Blocker
+ * Spam Blocker Behavior
  *
  * A CakePHP Behavior that moderates / validates comments to check for spam.
  * Validates based on a point system. High points is an automatic approval, where as low points is marked as spam or deleted.
  * Based on Jonathon Snooks outline.
  *
- * @author 		Miles Johnson - www.milesj.me
- * @copyright	Copyright 2006-2009, Miles Johnson, Inc.
- * @license 	http://www.opensource.org/licenses/mit-license.php - Licensed under The MIT License
- * @link		www.milesj.me/resources/script/commentia-behavior
- * @link		www.snook.ca/archives/other/effective_blog_comment_spam_blocker/
+ * @author      Miles Johnson - www.milesj.me
+ * @copyright   Copyright 2006-2010, Miles Johnson, Inc.
+ * @license     http://www.opensource.org/licenses/mit-license.php - Licensed under The MIT License
+ * @link        http://milesj.me/resources/script/spam-blocker-behavior
+ * @link        http://snook.ca/archives/other/effective_blog_comment_spam_blocker/
  */
+
+/**
+CREATE TABLE `tests`.`comments` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `entry_id` INT NOT NULL,
+    `status` ENUM('approved','pending','delete','spam') NOT NULL DEFAULT 'pending',
+    `points` INT NOT NULL,
+    `name` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `website` VARCHAR(50) NOT NULL,
+    `content` TEXT NOT NULL,
+    `created` DATETIME NULL DEFAULT NULL,
+    `modified` DATETIME NULL DEFAULT NULL,
+    INDEX (`entry_id`)
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+*/
  
-class CommentiaBehavior extends ModelBehavior {
+class SpamBlockerBehavior extends ModelBehavior {
 
 	/**
-	 * Current version: www.milesj.me/resources/logs/commentia-behavior
+	 * Current version: http://milesj.me/resources/logs/spam-blocker-behavior
 	 *
 	 * @access public
 	 * @var string
 	 */ 
-	public $version = '1.5';
+	public $version = '1.6';
 	
 	/**
 	 * Settings initiliazed with the behavior.
@@ -35,13 +51,15 @@ class CommentiaBehavior extends ModelBehavior {
 		'column_email'		=> 'email',		// Column name for the authors email
 		'column_website'	=> 'website',	// Column name for the authors website 
 		'column_foreign_id'	=> 'entry_id',	// Column name of the foreign id that links to the article/entry/etc
+        'column_slug'       => 'slug',      // Column name for the slug in the entries table
 		'parent_model'		=> 'Entry',		// Model name of the parent article/entry/etc
 		'article_link'		=> '',			// Link to the parent article, use :id for the permalink id
+        'use_slug'          => false,       // To use a slug in the article link, use :slug
 		'notify_email'		=> '',			// Email address where the notify emails should go
 		'save_points'		=> true,		// Should the points be saved to the database?
 		'send_email'		=> true,		// Should you receive a notification email for each comment? 
 		'blacklist_keys'	=> '',			// List of blacklisted words within text blocks
-		'blacklist_words'	=> '',			// List of blacklisted words within URLs
+		'blacklist_chars'	=> '',			// List of blacklisted characters within URLs
 		'deletion'			=> -10			// How many points till the comment is deleted (negative)
 	);
 	
@@ -59,7 +77,7 @@ class CommentiaBehavior extends ModelBehavior {
 	 * @access public
 	 * @var array
 	 */
-	public $blacklistWords = array('.html', '.info', '?', '&', '.de', '.pl', '.cn');
+	public $blacklistCharacters = array('.html', '.info', '?', '&', '.de', '.pl', '.cn');
 	
 	/**
 	 * Startup hook from the model.
@@ -71,15 +89,15 @@ class CommentiaBehavior extends ModelBehavior {
 	 */
 	public function setup(&$Model, $settings = array()) {
 		if (!empty($settings) && is_array($settings)) {
-			$this->settings = array_merge($this->settings, $settings);
+			$this->settings = $settings + $this->settings;
 		}
 		
 		if (!empty($this->settings['blacklist_keys']) && is_array($this->settings['blacklist_keys'])) {
-			$this->blacklistKeywords = array_merge($this->blacklistKeywords, $this->settings['blacklist_keys']);
+			$this->blacklistKeywords = $this->settings['blacklist_keys'] + $this->blacklistKeywords;
 		}
 		
-		if (!empty($this->settings['blacklist_words']) && is_array($this->settings['blacklist_words'])) {
-			$this->blacklistWords = array_merge($this->blacklistWords, $this->settings['blacklist_words']);
+		if (!empty($this->settings['blacklist_chars']) && is_array($this->settings['blacklist_chars'])) {
+			$this->blacklistCharacters = $this->settings['blacklist_chars'] + $this->blacklistCharacters;
 		}
 	}
 
@@ -156,7 +174,7 @@ class CommentiaBehavior extends ModelBehavior {
 				// URL length
 				// -1 if more then 30 chars
 				foreach ($links as $link) {
-					foreach ($this->blacklistWords as $word) {
+					foreach ($this->blacklistCharacters as $word) {
 						if (stripos($link, $word) !== false) {
 							--$points;
 						}
@@ -176,7 +194,7 @@ class CommentiaBehavior extends ModelBehavior {
 				// Body starts with...
 				// -10 points
 				$firstWord = mb_substr($data[$this->settings['column_content']], 0, stripos($data[$this->settings['column_content']], ' '));
-				$firstDisallow = array_merge($this->blacklistKeywords, array('interesting', 'cool', 'sorry'));
+				$firstDisallow = array('interesting', 'cool', 'sorry') + $this->blacklistKeywords;
 				
 				if (in_array(mb_strtolower($firstWord), $firstDisallow)) {
 					$points = $points - 10;
@@ -223,19 +241,17 @@ class CommentiaBehavior extends ModelBehavior {
 				if ($status == 'delete') {
 					$Model->delete($Model->id, false);
 				} else {
-					$update = array();
-					$update['status'] = $status;
-					$update['points'] = $points;
-					
-					$save = array('status');
-					if ($this->settings['save_points'] === true) {
-						$save[] = 'points';
+					$update = array('status' => $status);
+
+					if ($this->settings['save_points']) {
+						$update['points'] = $points;
 					}
 					
-					$Model->id = $Model->id;
-					$Model->save($update, false, $save);
+					$Model->save($update, false, array_keys($update));
 					
-					if ($this->settings['send_email'] === true) {
+					if ($this->settings['send_email']) {
+                        $update['points'] = $points;
+                        
 						$this->__notify($data, $update);
 					}
 				}		
@@ -249,26 +265,34 @@ class CommentiaBehavior extends ModelBehavior {
 	 * Sends out an email notifying you of a new comment.
 	 * 
 	 * @access private
-	 * @uses EmailComponent, Model
+	 * @uses Model
 	 * @param array $data
 	 * @param array $stats
 	 * @return void
 	 */
 	private function __notify($data, $stats) {
 		if (!empty($this->settings['parent_model']) && !empty($this->settings['article_link']) && !empty($this->settings['notify_email'])) {
-			App::import('Component', 'Email');
-			$Email = new EmailComponent();
 			$Entry = ucfirst(mb_strtolower($this->settings['parent_model']));
 			
 			// Get parent entry/blog
-			$entry = ClassRegistry::init($Entry)->find('first', array(
-				'fields' => array($Entry .'.id', $Entry .'.title'),
-				'conditions' => array($Entry .'.id' => $data[$this->settings['column_foreign_id']])
-			));
-			
-			// Config
-			$entryLink 	= str_replace(':id', $entry[$Entry]['id'], $this->settings['article_link']);
-			$entryTitle = $entry[$Entry]['title'];
+            $fields = array('id', 'title');
+            if ($this->settings['use_slug']) {
+                $fields[] = $this->settings['column_slug'];
+            }
+
+            $entry = ClassRegistry::init($Entry)->find('first', array(
+                'fields' => $fields,
+                'conditions' => array('id' => $data[$this->settings['column_foreign_id']]),
+                'recursive' => -1,
+                'contain' => false
+            ));
+
+            if ($this->settings['use_slug']) {
+                $entryLink 	= str_replace(':slug', $entry[$Entry][$this->settings['column_slug']], $this->settings['article_link']);
+            }
+
+            $entryLink 	= str_replace(':id', $entry[$Entry]['id'], $this->settings['article_link']);
+            $entryTitle = $entry[$Entry]['title'];
 			
 			// Build message
 			$message  = "A new comment has been posted for: ". $entryLink ."\n\n";
@@ -277,10 +301,7 @@ class CommentiaBehavior extends ModelBehavior {
 			$message .= "Message:\n\n". $data[$this->settings['column_content']];
 			
 			// Send email
-			$Email->to = $this->settings['notify_email'];
-			$Email->from = $data[$this->settings['column_author']] .' <'. $data[$this->settings['column_email']] .'>';
-			$Email->subject = 'Comment Approval: '. $entryTitle;
-			$Email->send($message); 
+            mail($this->settings['notify_email'], 'Comment Approval: '. $entryTitle, $message, 'From: '. $data[$this->settings['column_author']] .' <'. $data[$this->settings['column_email']] .'>');
 		}
 	}
 	
