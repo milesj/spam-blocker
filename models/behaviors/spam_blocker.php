7544841e273aef841092470308b7d99e4df52608
Miles Johnson <mileswjohnson@gmail.com>
2010-08-16 23:59:18 -0700

    Added further custom support for column names and status codes.

--- models/behaviors/spam_blocker.php
+++ models/behaviors/spam_blocker.php
@@ -1,5 +1,5 @@
 <?php
-/** 
+/**
  * Spam Blocker Behavior
  *
  * A CakePHP Behavior that moderates / validates comments to check for spam.
@@ -28,7 +28,7 @@ CREATE TABLE `tests`.`comments` (
     INDEX (`entry_id`)
 ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 */
- 
+
 class SpamBlockerBehavior extends ModelBehavior {
 
     /**
@@ -46,12 +46,6 @@ class SpamBlockerBehavior extends ModelBehavior {
      * @var array
      */
     public $settings = array(
-        'column_author'     => 'name',      // Column name for the authors name
-        'column_content'    => 'content',   // Column name for the comments body
-        'column_email'      => 'email',     // Column name for the authors email
-        'column_website'    => 'website',   // Column name for the authors website
-        'column_foreign_id' => 'entry_id',  // Column name of the foreign id that links to the article/entry/etc
-        'column_slug'       => 'slug',      // Column name for the slug in the entries table
         'parent_model'      => 'Entry',     // Model name of the parent article/entry/etc
         'article_link'      => '',          // Link to the parent article, use :id for the permalink id
         'use_slug'          => false,       // To use a slug in the article link, use :slug
@@ -61,6 +55,37 @@ class SpamBlockerBehavior extends ModelBehavior {
         'blacklist_keys'    => '',          // List of blacklisted words within text blocks
         'blacklist_chars'   => '',          // List of blacklisted characters within URLs
         'deletion'          => -10          // How many points till the comment is deleted (negative)
+    );
+
+    /**
+     * Names for table columns within the comments table and the parent.
+     *
+     * @access public
+     * @var array
+     */
+    public $columns = array(
+        'author'        => 'name',      // Column for the authors name
+        'content'       => 'content',   // Column for the comments body
+        'email'         => 'email',     // Column for the authors email
+        'website'       => 'website',   // Column for the authors website
+        'foreign_id'    => 'entry_id',  // Column of the foreign id that links to the article/entry/etc
+        'slug'          => 'slug',      // Column for the slug in the entries table
+        'title'         => 'title',     // Column for article title in the entries table
+        'status'        => 'status',    // Column for approval status
+        'points'        => 'points'     // Column for point score
+    );
+
+    /**
+     * Status codes for the enum (or integer) status column.
+     *
+     * @access public
+     * @var array
+     */
+    public $statusCodes = array(
+        'pending'   => 'pending',
+        'approved'  => 'approved',
+        'delete'    => 'denied',
+        'spam'      => 'spam'
     );
 
     /**
@@ -89,7 +114,17 @@ class SpamBlockerBehavior extends ModelBehavior {
      */
     public function setup(&$Model, $settings = array()) {
         if (!empty($settings) && is_array($settings)) {
-            $this->settings = $settings + $this->settings;
+            if (!empty($settings['settings'])) {
+                $this->settings = $settings['settings'] + $this->settings;
+            }
+
+            if (!empty($settings['columns'])) {
+                $this->columns = $settings['columns'] + $this->columns;
+            }
+
+            if (!empty($settings['statusCodes'])) {
+                $this->statusCodes = $settings['statusCodes'] + $this->statusCodes;
+            }
         }
 
         if (!empty($this->settings['blacklist_keys']) && is_array($this->settings['blacklist_keys'])) {
@@ -116,11 +151,11 @@ class SpamBlockerBehavior extends ModelBehavior {
 
             if (!empty($data)) {
                 // Get links in the content
-                $links = preg_match_all("#(^|[\n ])(?:(?:http|ftp|irc)s?:\/\/|www.)(?:[-A-Za-z0-9]+\.)+[A-Za-z]{2,4}(?:[-a-zA-Z0-9._\/&=+%?;\#]+)#is", $data[$this->settings['column_content']], $matches);
+                $links = preg_match_all("#(^|[\n ])(?:(?:http|ftp|irc)s?:\/\/|www.)(?:[-A-Za-z0-9]+\.)+[A-Za-z]{2,4}(?:[-a-zA-Z0-9._\/&=+%?;\#]+)#is", $data[$this->columns['content']], $matches);
                 $links = $matches[0];
 
                 $totalLinks = count($links);
-                $length = mb_strlen($data[$this->settings['column_content']]);
+                $length = mb_strlen($data[$this->columns['content']]);
 
                 // How many links are in the body
                 // +2 if less than 2, -1 per link if over 2
@@ -143,19 +178,19 @@ class SpamBlockerBehavior extends ModelBehavior {
                 // Number of previous comments from email
                 // +1 per approved, -1 per spam
                 $comments = $Model->find('all', array(
-                    'fields' => array($Model->alias .'.id', $Model->alias .'.status'),
-                    'conditions' => array($Model->alias .'.'. $this->settings['column_email'] => $data[$this->settings['column_email']]),
+                    'fields' => array('id', $this->columns['status']),
+                    'conditions' => array($this->columns['email'] => $data[$this->columns['email']]),
                     'recursive' => -1,
                     'contain' => false
                 ));
 
                 if (!empty($comments)) {
                     foreach ($comments as $comment) {
-                        if ($comment[$Model->alias]['status'] == 'spam') {
+                        if ($comment[$Model->alias][$this->columns['status']] == $this->statusCodes['spam']) {
                             --$points;
                         }
 
-                        if ($comment[$Model->alias]['status'] == 'approved') {
+                        if ($comment[$Model->alias][$this->columns['status']] == $this->statusCodes['approved']) {
                             ++$points;
                         }
                     }
@@ -164,7 +199,7 @@ class SpamBlockerBehavior extends ModelBehavior {
                 // Keyword search
                 // -1 per blacklisted keyword
                 foreach ($this->blacklistKeywords as $keyword) {
-                    if (stripos($data[$this->settings['column_content']], $keyword) !== false) {
+                    if (stripos($data[$this->columns['content']], $keyword) !== false) {
                         --$points;
                     }
                 }
@@ -193,7 +228,7 @@ class SpamBlockerBehavior extends ModelBehavior {
 
                 // Body starts with...
                 // -10 points
-                $firstWord = mb_substr($data[$this->settings['column_content']], 0, stripos($data[$this->settings['column_content']], ' '));
+                $firstWord = mb_substr($data[$this->columns['content']], 0, stripos($data[$this->columns['content']], ' '));
                 $firstDisallow = array('interesting', 'cool', 'sorry') + $this->blacklistKeywords;
 
                 if (in_array(mb_strtolower($firstWord), $firstDisallow)) {
@@ -202,14 +237,14 @@ class SpamBlockerBehavior extends ModelBehavior {
 
                 // Author name has http:// in it
                 // -2 points
-                if (stripos($data[$this->settings['column_author']], 'http://') !== false) {
+                if (stripos($data[$this->columns['author']], 'http://') !== false) {
                     $points = $points - 2;
                 }
 
                 // Body used in previous comment
                 // -1 per exact comment
                 $previousComments = $Model->find('count', array(
-                    'conditions' => array($Model->alias .'.'. $this->settings['column_content'] => $data[$this->settings['column_content']]),
+                    'conditions' => array($this->columns['content'] => $data[$this->columns['content']]),
                     'recursive' => -1,
                     'contain' => false
                 ));
@@ -220,7 +255,7 @@ class SpamBlockerBehavior extends ModelBehavior {
 
                 // Random character match
                 // -1 point per 5 consecutive consonants
-                $consonants = preg_match_all('/[^aAeEiIoOuU\s]{5,}+/i', $data[$this->settings['column_content']], $matches);
+                $consonants = preg_match_all('/[^aAeEiIoOuU\s]{5,}+/i', $data[$this->columns['content']], $matches);
                 $totalConsonants = count($matches[0]);
 
                 if ($totalConsonants > 0) {
@@ -230,28 +265,28 @@ class SpamBlockerBehavior extends ModelBehavior {
 
             // Finalize and save
             if ($points >= 1) {
-                $status = 'approved';
+                $status = $this->statusCodes['approved'];
             } else if ($points == 0) {
-                $status = 'pending';
+                $status = $this->statusCodes['pending'];
             } else if ($points <= $this->settings['deletion']) {
-                $status = 'delete';
+                $status = $this->statusCodes['delete'];
             } else {
-                $status = 'spam';
+                $status = $this->statusCodes['spam'];
             }
 
-            if ($status == 'delete') {
+            if ($status == $this->statusCodes['delete']) {
                 $Model->delete($Model->id, false);
             } else {
-                $update = array('status' => $status);
+                $update = array($this->columns['status'] => $status);
 
                 if ($this->settings['save_points']) {
-                    $update['points'] = $points;
+                    $update[$this->columns['points']] = $points;
                 }
 
                 $Model->save($update, false, array_keys($update));
 
                 if ($this->settings['send_email']) {
-                    $update['points'] = $points;
+                    $update[$this->columns['points']] = $points;
 
                     $this->__notify($data, $update);
                 }
@@ -280,33 +315,33 @@ class SpamBlockerBehavior extends ModelBehavior {
             }
 
             // Get parent entry/blog
-            $fields = array('id', 'title');
+            $fields = array('id', $this->columns['title']);
             if ($this->settings['use_slug']) {
-                $fields[] = $this->settings['column_slug'];
+                $fields[] = $this->columns['slug'];
             }
 
             $entry = ClassRegistry::init($this->settings['parent_model'])->find('first', array(
                 'fields' => $fields,
-                'conditions' => array('id' => $data[$this->settings['column_foreign_id']]),
+                'conditions' => array('id' => $data[$this->columns['foreign_id']]),
                 'recursive' => -1,
                 'contain' => false
             ));
 
             if ($this->settings['use_slug']) {
-                $entryLink = str_replace(':slug', $entry[$Entry][$this->settings['column_slug']], $this->settings['article_link']);
+                $entryLink = str_replace(':slug', $entry[$Entry][$this->columns['slug']], $this->settings['article_link']);
             }
 
             $entryLink = str_replace(':id', $entry[$Entry]['id'], $this->settings['article_link']);
-            $entryTitle = $entry[$Entry]['title'];
+            $entryTitle = $entry[$Entry][$this->columns['title']];
 
             // Build message
             $message  = "A new comment has been posted for: ". $entryLink ."\n\n";
-            $message .= 'Name: '. $data[$this->settings['column_author']] .' <'. $data[$this->settings['column_email']] .">\n";
-            $message .= 'Status: '. ucfirst($stats['status']) .' ('. $stats['points'] ." Points)\n";
-            $message .= "Message:\n\n". $data[$this->settings['column_content']];
+            $message .= "Name: ". $data[$this->columns['author']] ." <". $data[$this->columns['email']] .">\n";
+            $message .= "Status: ". ucfirst($stats['status']) ." (". $stats['points'] ." Points)\n";
+            $message .= "Message:\n\n". $data[$this->columns['content']];
 
             // Send email
-            mail($this->settings['notify_email'], 'Comment Approval: '. $entryTitle, $message, 'From: '. $data[$this->settings['column_author']] .' <'. $data[$this->settings['column_email']] .'>');
+            mail($this->settings['notify_email'], 'Comment Approval: '. $entryTitle, $message, 'From: '. $data[$this->columns['author']] .' <'. $data[$this->columns['email']] .'>');
         }
     }
 
